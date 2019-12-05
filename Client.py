705ea8b9a823420e39dec9e0e4da61d6a4da19b7
Josie Bealle <josiebealle@gmail.com>
2019-12-05 15:43:35 +0100

    jfjf

--- Client.py
+++ Client.py
@@ -5,24 +5,23 @@ import json;
 
 
 
-resp = req.get("https://aqueous-dusk-24314.herokuapp.com/ip/all", timeout=20)
+def main():
 
-#Stock IP addresses in a list
-li = []
-res = resp.text ;
-loaded_json = json.loads(res);
-for  x in loaded_json :
-   li.append(x['address']);
+    resp = req.get("https://aqueous-dusk-24314.herokuapp.com/ip/all", timeout=20)
 
-"""
-traceroutes = []
-for address in li:
-    chemin= []
-    chemin.append(address) ;
-    for address_dst in li:
-        if(address_dst != address):
-            for i in range(1, 28):
-                pkt = IP(dst=address, ttl=i) / UDP(dport=33434)
+    #Stock IP addresses in a list
+    li = []
+    res = resp.text
+    loaded_json = json.loads(res)
+    for  x in loaded_json :
+       li.append(x['address'])
+
+
+    traceroutes = []
+    for address in li:
+        chemin= []
+        for i in range(1, 28):
+                pkt = IP(dst=address, ttl=i)
                 # Send the packet and get a reply
                 reply = sr1(pkt, verbose=0)
                 if reply is None:
@@ -36,28 +35,33 @@ for address in li:
                     # We're in the middle somewhere
                     print("%d hops away: " % i, reply.src)
                     chemin.append(reply.src)
+        print(chemin)
+        traceroutes.append(chemin)
 
 
-"""
+# def main() :
+#     hostname = "google.com"
+#     chemin=[];
+#     for i in range(1, 28):
+#         pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)
+#         # Send the packet and get a reply
+#         reply = sr1(pkt, verbose=0)
+#         if reply is None:
+#             print("none")
+#             # No reply =(
+#             break
+#         elif reply.type == 3:
+#             # We've reached our destination
+#             print("Done!", reply.src)
+#             break
+#         else:
+#             # We're in the middle somewhere
+#             print( "%d hops away: " % i , reply.src)
+#             chemin.append(reply.src)
+#
+#
+#     print(chemin)
 
-hostname = "google.com"
-chemin=[];
-for i in range(1, 28):
-    pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)
-    # Send the packet and get a reply
-    reply = sr1(pkt, verbose=0)
-    if reply is None:
-        # No reply =(
-        break
-    elif reply.type == 3:
-        # We've reached our destination
-        print("Done!", reply.src)
-        break
-    else:
-        # We're in the middle somewhere
-        print( "%d hops away: " % i , reply.src)
-        chemin.append(reply.src)
-
-
-print(chemin)
+if __name__ == '__main__':
+    main()
 
