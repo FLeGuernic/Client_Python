e59946766d979ca691fa506252c9e6b01f273434
Josie Bealle <josiebealle@gmail.com>
2019-12-09 19:20:44 +0100

    traceroute

--- Client.py
+++ Client.py
@@ -2,31 +2,34 @@
 from scapy.layers.inet import *
 import requests as req
 import json;
-
+from requests import get, post
 
 
 def main():
 
     resp = req.get("https://aqueous-dusk-24314.herokuapp.com/ip/all", timeout=20)
 
-    #Stock IP addresses in a list
     li = []
     res = resp.text
     loaded_json = json.loads(res)
-    for  x in loaded_json :
+    for x in loaded_json :
        li.append(x['address'])
 
-
     traceroutes = []
+
+    ip = get('https://api.ipify.org').text
+    print(ip)
     for address in li:
         chemin= []
-        for i in range(1, 28):
-                pkt = IP(dst=address, ttl=i)
+        print("dst: " + address)
+        for i in range(1, 20):
+                pkt = IP(dst=address, ttl=i) / UDP(dport=33434)
                 # Send the packet and get a reply
-                reply = sr1(pkt, verbose=0)
+                reply = sr1(pkt, verbose=0, timeout=10)
                 if reply is None:
                     # No reply =(
-                    break
+                    print("no reply")
+                    # break
                 elif reply.type == 3:
                     # We've reached our destination
                     print("Done!", reply.src)
@@ -36,31 +39,11 @@ def main():
                     print("%d hops away: " % i, reply.src)
                     chemin.append(reply.src)
         print(chemin)
-        traceroutes.append(chemin)
+
+        traceroutes.append({'src': ip, "dst": address, "route": chemin})
+    post(url="https://aqueous-dusk-24314.herokuapp.com/traceroute/", data={"traceroutes": traceroutes})
 
 
-# def main() :
-#     hostname = "google.com"
-#     chemin=[];
-#     for i in range(1, 28):
-#         pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)
-#         # Send the packet and get a reply
-#         reply = sr1(pkt, verbose=0)
-#         if reply is None:
-#             print("none")
-#             # No reply =(
-#             break
-#         elif reply.type == 3:
-#             # We've reached our destination
-#             print("Done!", reply.src)
-#             break
-#         else:
-#             # We're in the middle somewhere
-#             print( "%d hops away: " % i , reply.src)
-#             chemin.append(reply.src)
-#
-#
-#     print(chemin)
 
 if __name__ == '__main__':
     main()
