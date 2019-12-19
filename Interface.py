d0437d1adfe8d5e92f3608357416dd3a2a7642b6
Josie Bealle <josiebealle@gmail.com>
2019-12-19 09:33:11 +0100

    connect client to interface

--- Interface.py
+++ Interface.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import Client
+from Client import Client
 
 root = Tk()
 root.geometry("750x750")
@@ -8,13 +8,13 @@ fup = Frame(root)
 fup.pack(side=TOP)
 titre=Label(fup,text="Ping Athena",font=("Times",20,"bold"))
 titre.pack(side=TOP)
-img=PhotoImage(file="network.png")
-image=Label(fup,image=img)
-image.pack(side=BOTTOM,pady=30)
+# img=PhotoImage(file="./network.png")
+# image=Label(fup,image=img)
+# image.pack(side=BOTTOM,pady=30)
 
 fdown=Frame(root)
 fdown.pack(side=BOTTOM)
-button=Button(fdown,text="Launch traceroutes",command=Client.main())
+button=Button(fdown,text="Launch traceroutes",command=Client().run)
 button.pack(side=BOTTOM)
 
 root.mainloop()
